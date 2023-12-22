e0e2a450cd9ed2cdfb59c563b01ae73ee1a80ae9
MarionR6 <marionrenuy@gmail.com>
2023-12-22 11:13:03 +0100

    second commit

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -3,7 +3,7 @@ const API_RECIPES = "https://etoile-sucree-back.vercel.app/api/recipes";
 // FRONT FUNCTION TO CALL BACK REQUEST TO LIKE OR DISLIKE A RECIPE
 
 export async function toggleLikeRecipe(idRecipe, idUser) {
-    const response = await fetch(`https://etoile-sucree-back.vercel.app${API_RECIPES}/likeRecipe/${idUser}`, {
+    const response = await fetch(`${API_RECIPES}/likeRecipe/${idUser}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -19,7 +19,7 @@ export async function toggleLikeRecipe(idRecipe, idUser) {
 // FUNCTION FOR THE ADMIN TO DELETE A RECIPE
 
 export async function deleteRecipe(idRecipe) {
-    const response = await fetch(`https://etoile-sucree-back.vercel.app${API_RECIPES}/deleteRecipe`, {
+    const response = await fetch(`${API_RECIPES}/deleteRecipe`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json"
